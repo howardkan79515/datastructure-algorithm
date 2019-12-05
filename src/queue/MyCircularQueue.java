bd29b644638c59dcdd4b1e2faea059812e48c752
howardkan79515 <howardkan79515@gmail.com>
2019-12-06 00:06:35 +0800

    Implemented Design Circular Queue.

--- src/queue/MyCircularQueue.java
+++ src/queue/MyCircularQueue.java
@@ -1,118 +1,99 @@
 package queue;
 
+import java.util.Arrays;
+
 public class MyCircularQueue {
 	
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		MyCircularQueue queue = new MyCircularQueue(3);
-//		queue.enQueue(1);
-//		queue.enQueue(2);
-//		queue.enQueue(3);
-//		queue.enQueue(4);
-//		queue.Rear();
-//		queue.deQueue();
-//		queue.enQueue(5);
-//		queue.enQueue(6);
-//		queue.enQueue(7);
-//		queue.enQueue(8);
-//		queue.deQueue();
-//		
-//		queue.enQueue(3);
-//		queue.Front();
-//		queue.deQueue();
-//		queue.enQueue(3);
-//		queue.deQueue();
-//		
-//		queue.Front();
-//		queue.enQueue(1);
-//		queue.enQueue(2);
-//		queue.enQueue(3);
-//		queue.enQueue(4);
-//		queue.Rear();
-		
-		queue.enQueue(2);
-		queue.Rear();
-		queue.Front();
-		queue.deQueue();
-		queue.Front();
-		queue.deQueue();
-		queue.Front();
-		queue.enQueue(4);
-		queue.enQueue(2);
+		System.out.print(queue.Rear());
+		System.out.print(queue.Front());
+		queue.enQueue(1);
 		queue.enQueue(2);
 		queue.enQueue(3);
-
+		queue.enQueue(4);
+		queue.deQueue();
+		queue.deQueue();
+		queue.enQueue(4);
+		queue.enQueue(5);
+		queue.enQueue(6);
+		queue.enQueue(7);
+		queue.enQueue(8);
 	}
 	
-	int[] queue; 
+	int[] queue;
 	
-	int tail;
+	int head = 1;
 	
-	int head;
+	int tail = 1;
 	
-	 /** Initialize your data structure here. Set the size of the queue to be k. */
+
+    /** Initialize your data structure here. Set the size of the queue to be k. */
     public MyCircularQueue(int k) {
-        this.queue = new int[k];
-        this.tail = 0;
-        this.head = 0;
+        queue = new int[k+1];
+        Arrays.fill(queue, -1);
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
     public boolean enQueue(int value) {
     	if(isFull()) {
-    		return false;
+    		return false;   
     	}
-    	if(tail == queue.length && head != 0) {
-    		tail = 0;
-    		queue[tail] = value;
+    	queue[tail] = value; 
+    	if(tail < queue.length-1) {
     		tail++;
-    		return true;
+    	} else {
+    		tail = 0;
     	}
-    	queue[tail] = value;
-    	tail++;
 		return true;       
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
-    	if(tail == head) {
+    	if(head == tail) { 
     		return false;
     	}
-    	queue[head] = 0;
-    	head++;
-		return true;        
+    	queue[head] = -1;
+    	if(head < queue.length-1) {
+    		head++;
+    	} else {
+    		head = 0;
+    	}
+		return true;       
     }
     
     /** Get the front item from the queue. */
     public int Front() {
-    	if(tail == head) {
+    	if(isEmpty()) {
     		return -1;
     	}
-    	int result = queue[head];
-		return result;        
+		return queue[head];       
     }
     
     /** Get the last item from the queue. */
     public int Rear() {
-    	int result = queue[tail-1];
-		return result;
-        
+    	if(isEmpty()) {
+    		return -1;
+    	}
+    	int index = tail == 0 ? 
+    			queue.length-1 : tail-1;
+		return queue[index];        
     }
     
     /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
-    	if(tail == head) {
+    	if(head == tail) {
+    		return true;
+    	}
+		return false;       
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+    	if(((tail+1) % queue.length) == head) {
     		return true;
     	}
 		return false;
         
     }
-    
-    /** Checks whether the circular queue is full or not. */
-    public boolean isFull() {
-    	if(head == (tail+1) || tail == queue.length && head == 0) {
-    		return true;
-    	}
-		return false;       
-    }
-
 }
