66153f3ac057196428654fcf7f746f844bf42411
howardkan79515 <howardkan79515@gmail.com>
2020-11-16 23:26:06 +0800

    Howard Kang. Implement by Deque.

--- src/heap/SlidingWindowMaximum.java
+++ src/heap/SlidingWindowMaximum.java
@@ -1,32 +1,54 @@
 package heap;
 
+
 import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+
 import java.util.PriorityQueue;
 
 public class SlidingWindowMaximum {
 	
 	public static void main(String[] args) {
 		new SlidingWindowMaximum().maxSlidingWindow(new int[] {1,3,-1,-3,5,3,6,7}, 3);
+		new MaxSlidingWindow().maxSlidingWindow(new int[] {-7,-8,7,5,7,1,6,0},4);
 	}
 	
 	public int[] maxSlidingWindow(int[] nums, int k) {
-		if(nums.length == 0) {
-			return nums;
+		int[] result = new int[nums.length-k+1];
+		Deque<Integer> deque = new LinkedList<>();
+		for(int i = 0 ; i < nums.length ; i++) {
+			if(i >= k && !deque.isEmpty() && nums[i-k] == deque.peek()) {
+				deque.pollFirst();
+			}			
+			while(!deque.isEmpty() && deque.peekLast() < nums[i]) {
+				deque.removeLast();
+			}		
+			deque.add(nums[i]);		
+			if(i >= (k-1)) {
+				result[i-k+1] = deque.peek();
+			}
 		}
-		int[] result = new int[nums.length-(k-1)];
-		PriorityQueue<Integer> queue = new PriorityQueue<Integer>(
-				Collections.reverseOrder());
-		for(int i = 0 ; i < k ; i++) {
-			queue.add(nums[i]);
-		} 
-		for(int i = k ; i < nums.length ; i++) {
-			int formerMax = queue.peek();
-			result[i-k] = formerMax;
-			queue.remove(nums[i-k]);
-			queue.add(nums[i]);
+		return result;		
+	}
+	
+	public int[] maxSlidingWindowI(int[] nums, int k) {
+		PriorityQueue<Integer> heap = new PriorityQueue<>(
+				k, Collections.reverseOrder());	
+		int[] result = new int[nums.length-k+1]; 
+		for(int i = 0 ; i < nums.length ; i++) {
+			if(heap.size() != k) {
+				heap.add(nums[i]);
+				continue;
+			} 
+			Integer max = heap.peek();
+			result[i-k] = max;	
+			heap.add(nums[i]);
+			int removeVal = nums[i-k];
+			heap.remove(removeVal);		
 		}
-		result[nums.length-k] = queue.peek();
-		return nums;       
-    }
+		result[result.length-1] = heap.peek();
+		return result;		
+	}
 
 }
