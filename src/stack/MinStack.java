bfe499e2f53e5f30076f345f2ac10ea030a61cc6
howardkan79515 <howardkan79515@gmail.com>
2021-01-12 22:09:34 +0800

    Howard Kang. Modified coding style.

--- src/stack/MinStack.java
+++ src/stack/MinStack.java
@@ -3,53 +3,53 @@ package stack;
 import java.util.Arrays;
 
 public class MinStack {
-	
-	int[] stack;
-    
-    int min = Integer.MAX_VALUE;
-    
-    int index = -1;
 
-    /** initialize your data structure here. */
-    public MinStack() {
-        this.stack = new int[10];
-    }
-    
-    //Time complexity O(1)
-    public void push(int x) {
-        if(x < min) {
-            min = x;
-        }
-        index++;
-        if(index > stack.length-1){
-            stack = Arrays.copyOf(stack, stack.length*2);
-        }
-        stack[index] = x;       
-    }
-    
-    //Time complexity O(N)
-    public void pop() {
-        stack[index] = 0;
-        index--;
-        setMin();
-    }
+	int[] stack;
+
+	int min = Integer.MAX_VALUE;
+
+	int index = -1;
+
+	/** initialize your data structure here. */
+	public MinStack() {
+		this.stack = new int[10];
+	}
+
+	//Time complexity O(1)
+	public void push(int x) {
+		if(x < min) {
+			min = x;
+		}
+		index++;
+		if(index > stack.length-1){
+			stack = Arrays.copyOf(stack, stack.length*2);
+		}
+		stack[index] = x;       
+	}
+
+	//Time complexity O(N)
+	public void pop() {
+		stack[index] = 0;
+		index--;
+		setMin();
+	}
 
 	private void setMin() {
 		this.min = Integer.MAX_VALUE;
-        for(int i = 0 ; i <= index ; i++) {
-            if(stack[i] < min) {
-                min = stack[i];
-            }
-        }
+		for(int i = 0 ; i <= index ; i++) {
+			if(stack[i] < min) {
+				min = stack[i];
+			}
+		}
 	}
-    
-    public int top() {
-        return stack[index];
-    }
-    
-    //Time complexity O(1)
-    public int getMin() {
-        return this.min;
-    }
+
+	public int top() {
+		return stack[index];
+	}
+
+	//Time complexity O(1)
+	public int getMin() {
+		return this.min;
+	}
 
 }
--- src/stack/MinStackII.java
+++ src/stack/MinStackII.java
@@ -4,45 +4,45 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class MinStackII {
-	
-	
+
+
 	int index = -1;
-	
+
 	int[] stack;
-	
+
 	PriorityQueue<Integer> heap = new PriorityQueue<>();
 
 	public MinStackII() {
 		this.stack = new int[10];
 	}
-	
+
 	//Time complexity O(logN)
-    public void push(int x) {
-    	if(index >= stack.length-1) {
-    		this.stack = Arrays.copyOf(stack, stack.length*2);
-    	}
-    	index++;
-        stack[index] = x;  
-        heap.add(x);
-    }
-    
-    //Time complexity O(N)
-    public void pop() {
-        if(index == -1) {
-        	return;
-        }
-        heap.remove(stack[index]);
-        stack[index] = 0;
-        index--;
-    }
-    
-    public int top() {
+	public void push(int x) {
+		if(index >= stack.length-1) {
+			this.stack = Arrays.copyOf(stack, stack.length*2);
+		}
+		index++;
+		stack[index] = x;  
+		heap.add(x);
+	}
+
+	//Time complexity O(N)
+	public void pop() {
+		if(index == -1) {
+			return;
+		}
+		heap.remove(stack[index]);
+		stack[index] = 0;
+		index--;
+	}
+
+	public int top() {
 		return stack[index];       
-    }
-    
-    public int getMin() {
+	}
+
+	public int getMin() {
 		return heap.peek();       
-    }
-    
-    
+	}
+
+
 }
