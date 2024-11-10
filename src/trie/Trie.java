e64ec581ee7c9721a20ed855f621dd88410225c7
howardkan79515 <howardkan79515@gmail.com>
2024-11-10 16:14:56 +0800

    Commit a better solution for Trie

--- src/trie/Trie.java
+++ src/trie/Trie.java
@@ -10,103 +10,87 @@ public class Trie {
 	   trie.insert("app");
 	   trie.search("app");
    }
-	
-   TrieNode[] children;
-	
-	 /** Initialize your data structure here. */
-   public Trie() {
-   	children = new TrieNode[26];
-   }
-   
-   public void insert(String word) {
-   	char[] charArray = word.toCharArray();
-   	children = buildChildNodes(children, charArray, 0);
-   }
-   
-   private TrieNode[] buildChildNodes(TrieNode[] children, char[] charArray, int i) {
-	   if(i > charArray.length-1) {
-		   return null;
-	   }
-	   char value = charArray[i];
-	   int index = value - 'a';
-	   i++;
-	   children = children == null ? new TrieNode[26] : children;
-	   TrieNode node = children[index];
-	   if(node == null) {
-		   node = new TrieNode(value);
-		   children[index] = node;
-	   } 
-	   if(i == charArray.length) {
-		   node.isEndingChar = true;
-		   return children;
-	   }
-	   
-	   node.children = buildChildNodes(node.children, charArray, i);
-	   return children;
-   }
-   
-   /** Returns if the word is in the trie. */
-   public boolean search(String word) {
-	   char[] wordArray = word.toCharArray();
-	   boolean result = searchTrie(children, wordArray, 0);
-	   return result;        
-   }
-   
-   private boolean searchTrie(TrieNode[] children, char[] wordArray, int i) {
-	   if(i > wordArray.length-1) {
-		   return false;
-	   }
-	   char value = wordArray[i];
-	   int index = value - 'a';
-	   TrieNode node = children[index];
-	   i++;
-	   if(node == null) {
-		   return false;
-	   } else if (node.isEndingChar && i == wordArray.length){
-		   return true;
-	   } else if (node.children != null){
-		   return searchTrie(node.children, wordArray, i);
-	   }
-	   return false;
-   }
-   
-   public boolean startsWith(String prefix) {
-	   char[] prefixArray = prefix.toCharArray();
-	   boolean result = checkStartsWith(children, prefixArray, 0);
-	   return result;       
-   }
-   
-   private boolean checkStartsWith(TrieNode[] children, char[] wordArray, int i) {
-	   if(i > wordArray.length - 1) {
-		   return false;
-	   }
-	   char value = wordArray[i];
-	   int index = value - 'a';
-	   TrieNode node = children[index];
-	   i++;
-	   if(node == null) {
-		   return false;
-	   } else if(i == wordArray.length) {
-		   return true;
-	   } else if (node.children != null){
-		   return checkStartsWith(node.children, wordArray, i);
-	   }
-	   return false;
-   }
-   
-   public class TrieNode {
 
-	   char value;
+	public Trie() {
+	}
 
-	   TrieNode[] children = new TrieNode[26];
+	TrieNode[] nodes = new TrieNode[26];
 
-	   boolean isEndingChar = false;
+	public void insert(String word) {
+		insert(nodes, 0, word);
+	}
 
-	   public TrieNode(char value) {
-		   this.value = value;
-	   }
-	   
-   }
+	private void insert(TrieNode[] nodes, int i, String word) {
+		if (i >= word.length()) {
+			return;
+		}
+		char c = word.charAt(i);
+		int num = c - 'a';
+		TrieNode node = nodes[num];
+		if (node == null) {
+			node = new TrieNode(num);
+			nodes[num] = node;
+		}
+		if (i == word.length()-1) {
+			node.end = true;
+		}
+		insert(node.children, i+1, word);
+	}
+
+	public boolean search(String word) {
+		return search(nodes, word, 0);
+	}
+
+	private boolean search(TrieNode[] nodes, String word, int i) {
+		if (i >= word.length()) {
+			return false;
+		}
+		char c = word.charAt(i);
+		int num = c - 'a';
+		TrieNode node = nodes[num];
+		if (node == null) {
+			return false;
+		}
+		if (i == word.length()-1) {
+			if (node.end) {
+				return true;
+			}
+		}
+		return search(node.children, word, i+1);
+	}
+
+	public boolean startsWith(String prefix) {
+		return startsWith(nodes, prefix, 0);
+	}
+
+	private boolean startsWith(TrieNode[] nodes, String prefix, int i) {
+		if (i >= prefix.length()) {
+			return false;
+		}
+		char c = prefix.charAt(i);
+		int ind = c - 'a';
+		TrieNode node = nodes[ind];
+		if (node == null) {
+			return false;
+		} else if (node.value == -1){
+			return false;
+		} else if (node.value != -1 && i == prefix.length()-1){
+			return true;
+		}
+		return startsWith(node.children, prefix, i+1);
+	}
+
+	public class TrieNode {
+		boolean end = false;
+
+		int value = -1;
+
+		TrieNode[] children = new TrieNode[26];
+
+		TrieNode(int value) {
+			this.value = value;
+		}
+	}
    
 
 }
