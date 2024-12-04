27a3b764974da7dbbf5463aca59507734137bff4
howardkan79515 <howardkan79515@gmail.com>
2024-12-04 21:25:29 +0800

    Fix FloodFill

--- src/dfs/FloodFill.java
+++ src/dfs/FloodFill.java
@@ -8,23 +8,20 @@ public class FloodFill {
 	}
 
 	public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-		cache = new int[image.length][image[0].length];
 		int target = image[sr][sc];
 		dfs(image, sr, sc, color, target);
 		return image;
 	}
 
-	int[][] cache;
 
 	private void dfs(int[][] image, int sr, int sc, int color, int target) {
 		if (sr > image.length-1 || sc > image[0].length-1 || sr < 0 || sc < 0) {
 			return;
 		}
-		if (image[sr][sc] != target || cache[sr][sc] == 1) {
+		if (image[sr][sc] != target || image[sr][sc] == color) {
 			return;
 		}
 		image[sr][sc] = color;
-		cache[sr][sc] = 1;
 		dfs(image, sr+1, sc, color, target);
 		dfs(image, sr, sc+1, color, target);
 		dfs(image, sr-1, sc, color, target);
