3b3fa4e608ed430875c3677129991352d7ead23f
howardkan79515 <howardkan79515@gmail.com>
2024-12-08 18:09:21 +0800

    Commit DiameterOfBinaryTree

---
+++ src/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,30 @@
+package tree;
+
+public class DiameterOfBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right = new TreeNode(3);
+        new DiameterOfBinaryTree().diameterOfBinaryTree(root);
+    }
+
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        max = Math.max(max, left+right);
+        return Math.max(left, right)+1;
+    }
+}
