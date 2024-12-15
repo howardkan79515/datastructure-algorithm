301924cc081795ee8cf519e1dc750fec9c61dfe2
howardkan79515 <howardkan79515@gmail.com>
2024-12-15 20:25:57 +0800

    Reimplement WordSearch

--- src/dfs/WordSearch.java
+++ src/dfs/WordSearch.java
@@ -17,49 +17,45 @@ public class WordSearch {
 			{'S','F','E','S'},
 			{'A','D','E','E'}};
 			//char[][] board2 = new char[][]{{'a','b'},{'c','d'}};
-			new WordSearch().exist(board, "ABCESEEEFS");
+			new WordSearch().exist(board, "ABCEFSADEESE");
 	}
 
 
 	public boolean exist(char[][] board, String word) {
-		char[] array = word.toCharArray();
-		for(int i = 0 ; i < board.length ; i++) {
-			for(int j = 0 ; j < board[0].length ; j++) {
-				boolean[][] state = new boolean[board.length+1][board[0].length+1];
-				boolean result = dfs(board, array, i, j, 0, state);
-				if(result) {
-					return true;
+		char first = word.charAt(0);
+		char[] wArray = word.toCharArray();
+		for (int j = 0; j < board.length; j++) {
+			for (int i = 0; i < board[j].length; i++) {
+				if (first == board[j][i]) {
+					boolean[][] cache = new boolean[board.length][board[0].length];
+					if (dfs(board, wArray, i, j, 0, cache)) {
+						return true;
+					}
 				}
 			}
 		}
-		return false;        
-	}
-
-	private boolean dfs(char[][] board, char[] array, int x, int y, int i, boolean[][] state) {
-		if(!valid(board, x, y) || i > array.length-1 || state[x][y] || board[x][y] != array[i]) {
-			i = 0;
-			return false;
-		} 
-		i = i+1;
-		state[x][y] = true;
-		if(i == array.length) {
-			i = 0;
-			return true;
-		}
-		if(dfs(board, array, x+1, y, i, state)||
-				dfs(board, array, x-1, y, i, state)||
-				dfs(board, array, x, y+1, i, state)||
-				dfs(board, array, x, y-1, i, state)) {
-			return true;
-		}
-		state[x][y] = false;
 		return false;
 	}
 
-	private boolean valid(char[][] board, int x, int y) {
-		if(x > board.length-1 || y > board[0].length-1 || x < 0 || y < 0) {
+	private boolean dfs(char[][] board, char[] wArray, int i, int j, int k, boolean[][] cache) {
+		if (i >= board[0].length || j >= board.length || i < 0 || j < 0) {
 			return false;
-		}		
-		return true;		
+		}
+		if (board[j][i] != wArray[k]) {
+			return false;
+		}
+		if (cache[j][i]) {
+			return false;
+		}
+		if (k == wArray.length-1) {
+			return true;
+		}
+		cache[j][i] = true;
+		if (dfs(board, wArray, i+1, j, k+1, cache) || dfs(board, wArray, i, j+1, k+1, cache) ||
+				dfs(board, wArray, i-1, j, k+1, cache) || dfs(board, wArray, i, j-1,k+1, cache)) {
+			return true;
+		}
+		cache[j][i] = false;
+		return false;
 	}
 }
